574af2e733d3d210abc30fdae55cfce4a623bb95
Duco Dokter <dokter@w20e.com>
2014-01-07 21:13:09 +0100

    import fix for django 1.6 compat; removed customer from admin.py

--- mrwolfe/admin.py
+++ mrwolfe/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from models.issue import Issue
 from models.sla import SLA
-from models.customer import Customer
 
 
 class IssueAdmin(admin.ModelAdmin):
@@ -14,12 +13,6 @@ class SLAAdmin(admin.ModelAdmin):
     pass
 
 
-class CustomerAdmin(admin.ModelAdmin):
-
-    pass
-
-
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(SLA, SLAAdmin)
-admin.site.register(Customer, CustomerAdmin)
 

3d604d89c8672043db66118dd95bc77264bf64e6
Duco Dokter <dokter@w20e.com>
2014-03-14 10:33:36 +0100

    Fixed requiredness on chckboxes

--- mrwolfe/forms/comment.py
+++ mrwolfe/forms/comment.py
@@ -4,6 +4,9 @@ from mrwolfe.models.comment import Comment
 
 class CommentForm(forms.ModelForm):
 
+    no_notification = forms.BooleanField("Do not send notification",
+                                         required=False)
+
     class Meta:
         model = Comment
         widgets = {'issue': forms.HiddenInput()}
--- mrwolfe/forms/status.py
+++ mrwolfe/forms/status.py
@@ -4,8 +4,9 @@ from mrwolfe.models.status import Status
 
 class StatusForm(forms.ModelForm):
 
-    no_comment = forms.BooleanField("No comment")
-    no_notification = forms.BooleanField("Do not send notification")
+    no_comment = forms.BooleanField("No comment", required=False)
+    no_notification = forms.BooleanField("Do not send notification",
+                                         required=False)
 
     class Meta:
         model = Status
